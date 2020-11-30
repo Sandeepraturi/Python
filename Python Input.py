ecd61cb18760627f3f5a5e3d4ae2c0d31e65e558
Sandeep <s.raturi89@gmail.com>
2020-11-30 13:37:06 +0800

    Python input function

---
+++ Python Input.py
@@ -0,0 +1,16 @@
+# Example - 1: take two numbers input from users and print sum of two numbers
+print('\nExample 1:Program for sum of two numbers:-')
+first_number = int(input('Enter first number: '))
+second_number = int(input('Enter second number: '))
+print('Output: \nSum of {} and {} is :'.format(first_number, second_number), first_number+second_number)
+
+# Example - 2: Take input from user and display
+print('\nExample-2: Take input from user and display message')
+your_name = input('Enter your name: ')
+age = int(input('Enter you age: '))
+print('Hello, {}!, your age is {}.'.format(your_name, age))
+
+# Example - 3:
+print("\nExample - 3:\nEnter anything:")
+x1 = input()
+print("You entered : ", x1)
